61387c60f1f4f92ee6a0b2957f7683d6d56df351
angela <ngangaa@g.cofc.edu>
2021-09-03 16:58:20 -0400

    Completed hw 1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Angela Nganga
+hello.py
+
+Problem: This function prints "hello, world!".
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main ():
+    print("hello, world!")
+
+main ()
