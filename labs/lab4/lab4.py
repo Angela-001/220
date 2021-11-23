dc3788773debdf127afa98b763be3b8560954d54
angela <ngangaa@g.cofc.edu>
2021-11-23 18:19:57 -0500

    Re-did lab 4

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -1,7 +1,7 @@
 """
 Name: Angela Nganga
 lab4.py
-Problem: This function draws circles in locations given by the user.
+Problem: These functions use the graphics package and practice accumulating sequences.
 
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
@@ -11,8 +11,6 @@ from graphics import *
 
 
 def squares():
-
-
     # Creates a graphical window
     width = 400
     height = 400
@@ -27,15 +25,15 @@ def squares():
     instructions.draw(win)
 
     # builds a circle
-    shape = Rectangle(Point(50, 70), Point(90,110))
+    shape = Rectangle(Point(50, 70), Point(90, 110))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
 
     # allows the user to click multiple times to move the circle
     for i in range(num_clicks):
+        print("test")
         p = win.getMouse()
-        c = shape.getCenter()
 
         # center of circle
 
@@ -43,19 +41,17 @@ def squares():
         # point where the user clicked
         dx = p.getX()
         dy = p.getY()
-        # shape.move(dx, dy)
-        shape = Rectangle(Point((dx - 20, dy- 20), Point((dx + 20, dy + 20))))
-        shape.setOutline("red")
-        shape.setFill("red")
-        shape.draw(win)
+        new = Rectangle(Point(dx - 20, dy - 20), Point(dx + 20, dy + 20))
+        new.setOutline("red")
+        new.setFill("red")
+        new.draw(win)
 
     print("Click again to quit: ")
     win.getMouse()
     win.close()
 
-"""
-Problem: This function displays information about a rectangle drawn by the user.
-"""
+
+
 
 def rectangle():
     # Creates a graphical window
@@ -75,14 +71,14 @@ def rectangle():
     width = abs(width)
     height = corner1.getY() - corner2.getY()
     height = abs(height)
-    perimeter = 2*(width + height)
+    perimeter = 2 * (width + height)
     area = (width) * (height)
 
     instr_pt = Point(200, height - 20)
     perimeter = Text(instr_pt, "Display the perimeter: " + str(perimeter))
     perimeter.draw(win)
     instr_pt = Point(200, height - 40)
-    area = Text(instr_pt, "Display the area: " +str(area))
+    area = Text(instr_pt, "Display the area: " + str(area))
     area.draw(win)
 
     print("Click again to quit: ")
@@ -91,40 +87,46 @@ def rectangle():
 
     pass
 
-"""
-Problem: This function displays information about a rectangle drawn by the user.
-"""
+
 
 
 def circle():
-
-
     # Creates a graphical window
     width = 400
     height = 400
-    win = GraphWin("Lab 4")
+    win = GraphWin("Lab 4", width, height)
 
     center = win.getMouse()
-    circum= win.getMouse()
+    circum = win.getMouse()
 
-    radius = ((center.getX() - circum.getX())**2 + (center.getY() - circum.getY())**2)**(1/2)
+    radius = ((center.getX() - circum.getX()) ** 2 + (center.getY() - circum.getY()) ** 2) ** (1 / 2)
     # radius = abs(radius)
     circle = Circle(center, radius)
     circle.draw(win)
-    # d = ((radius)**)+()
     instr_pt = Point(width / 2, height - 10)
-    radius = Text(instr_pt, "Display the radius" + str(radius))
-    radius.draw(win)
-
-    print("Click again to quit: ")
+    radiustext = Text(instr_pt, "Radius is " + str(radius) + ", click again to quit.")
+    radiustext.draw(win)
     win.getMouse()
     win.close()
 
-"""
-Problem: This function approximate the value for pi.
-"""
+
+
+
 def pi2():
-    eval(input("Enter a value for n: "))
-    eval(input("Enter the numbers of terms to sum: "))
-    pi = pi+(V*switch)
-    switch = switch*(-1)
+    n = eval(input("Enter the numbers of terms to sum: "))
+    sum = 0.0
+    print(n)
+    for i in range(1, 4 * n, 4):
+        sum += (4.0 / i) - (4.0 / (i + 2.0))
+        print(sum)
+    print(sum)
+
+
+def main():
+    # squares()
+    # rectangle()
+    # circle()
+    # pi2()
+
+
+main()
